2737143800221ae79c56b569eac3fd9e7487f8d9
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-02 14:37:40 -0300

    Rename

--- src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
+++ src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
@@ -4,24 +4,24 @@ namespace WebApplication1.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class TasksExampleController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<TasksExampleController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public TasksExampleController(ILogger<TasksExampleController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Services> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Services
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
