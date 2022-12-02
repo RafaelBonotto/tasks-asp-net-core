db05f2bbf353711db027998f31f1a03d232b31a1
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-02 14:42:56 -0300

    Controller

--- src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
+++ src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
@@ -3,31 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("tasks")]
     public class TasksExampleController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        
+        [HttpGet("asynchronous")]
+        public async Task<IActionResult> GetTasks()
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<TasksExampleController> _logger;
-
-        public TasksExampleController(ILogger<TasksExampleController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<Services> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Services
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("");
         }
     }
 }
