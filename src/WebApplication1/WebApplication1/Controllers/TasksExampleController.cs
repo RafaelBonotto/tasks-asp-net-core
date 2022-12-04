b3a81f1b35a3adbc80acc969c2ead1adb62e97d4
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-12-04 17:29:50 -0300

    Tasks Assincronas

--- src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
+++ src/WebApplication1/WebApplication1/Controllers/TasksExampleController.cs
@@ -13,8 +13,25 @@ namespace WebApplication1.Controllers
 
         [HttpGet("asynchronous")]
         public async Task<IActionResult> GetTasksExample1()
-        {            
-            return Ok("");
+        {
+            List<Task> tasks = new ();    
+            List<string> response = new ();
+
+            Task task1 = _services.GetTaskOne();
+            Task task2 = _services.GetTaskTwo();
+
+            tasks.Add (task1);
+            tasks.Add (task2);
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                var result = ((Task<string>)task).Result;
+                response.Add(result);
+            }
+
+            return Ok(tasks);
         }
 
         
